7cd00e0348123d25e9f2ea704ea78e5ad9079471
Serik Kumarbek <s.kumarbek@mail.ru>
2025-07-31 17:14:39 +0500

    алюр есть и тест лого и faq работает

--- conftest.py
+++ conftest.py
@@ -1,13 +1,12 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()),options=options)
-    driver.set_window_size(1200, 1080)
+    options.add_argument("-private")
+
+    driver = webdriver.Firefox(options=options)
     yield driver
     driver.quit()
